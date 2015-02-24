6e6d3807b28ba9ac0d570aebf10d34df0219db3f
1protheroery <1protheroery@gws4000112.ihdsb.ca>
2015-02-24 10:56:18 -0500

    - initialized repository for first time

---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Name: HelloWorld.java
+ * Date: Feb 24th, 2015
+ * Version: v0.1
+ * Author: Ryan Protheroe
+ * Description: Simple program to display "hello world" to the screen.
+*/
+
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println( "Hello World" );
+        System.err.println( "Hello World" );
+        
+    }
+    
+}

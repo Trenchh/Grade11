bf217d2eb39e3215fa57b541763f7f542168c806
1protheroery <1protheroery@gws4000112.ihdsb.ca>
2015-02-25 10:56:33 -0500

    - Added bingo card example w/ table format

---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/Bingo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class Bingo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.format( "%5s %5s %5s %5s %5s\n", "B", "I", "N", "G", "O");
+        System.out.format( "%5s %5s %5s %5s %5s\n", "4", "16", "28", "49", "71");
+        System.out.format( "%5s %5s %5s %5s %5s\n", "3", "18", "26", "41", "78");
+        System.out.format( "%5s %5s %5s %5s %5s\n", "8", "22", "29", "44", "73");
+        System.out.format( "%5s %5s %5s %5s %5s\n", "7", "16", "28", "51", "71");
+        System.out.format( "%5s %5s %5s %5s %5s\n", "9", "23", "34", "54", "69");
+        
+        
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/BingoRules.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class BingoRules {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bingo Rules");
+        System.out.println("Bingo Rules");
+    }
+    
+}

c9d11bddc380b45d00b7ae241b1df269a8dd319d
Ryan <Ryan@192.168.2.14>
2015-02-25 21:11:47 -0500

    - Finished rules for Bingo - Created/finished rules for Rock Paper Scissors - Created unique smiley using keyboard characters

---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/RockPaperScissorsRules.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author Ryan
+ */
+public class RockPaperScissorsRules {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Rock Paper Scissors Rules");
+        System.out.println("Players decide on different hand signals to represent rock, paper, and scissors.");
+        System.out.println("Players make a throw at the same time.");
+        System.out.println("The hand signals thrown by the players are then compared to the rules of the game to determine the winner.");
+        System.out.println("    Rock dulls Scissors <Rock wins>.");
+        System.out.println("    Scissors cuts Paper <Scissors wins>.");
+        System.out.println("    Paper cover Rock <Paper wins>.");
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/SmileyFaceActivity.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author Ryan
+ */
+public class SmileyFaceActivity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("     *****");
+        System.out.println("    *_   _*");
+        System.out.println("   * 0   0 *");
+        System.out.println("  *    <    *");
+        System.out.println("    *  _  *");
+        System.out.println("     *****");
+    }
+    
+}

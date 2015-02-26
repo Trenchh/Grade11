ed593bf43217e0027c14c335008be75803605456
1protheroery <1protheroery@gws4000112.ihdsb.ca>
2015-02-26 11:08:49 -0500

    - Finished 3 Activities: Shortfall, Double Jeopardy, CharAssassination - Began Payroll

---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/CharAssassination.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class CharAssassination {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char ch = 'A' ;
+        System.out.println("A char: " +  ch );
+        
+        // Majority of the alternatives work except those who have two characters,
+        // double quatations, or no character at all due to char's standards
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/protheroe/ics3u/u3/DoubleJeopardy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.protheroe.ics3u.u3;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class DoubleJeopardy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        double value = 3200000000;
+        System.out.println("A double: " +  value);
+  }
+}

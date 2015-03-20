f2b636f6fa0da3f1a811a21642f03de58f6c0be3
Ryan <Ryan@192.168.2.14>
2015-03-20 19:20:05 -0400

    - Completed Order Checker

--- src/edu/hdsb/gwss/protheroe/ics3u/u4/OrderChecker.java
+++ src/edu/hdsb/gwss/protheroe/ics3u/u4/OrderChecker.java
@@ -3,7 +3,7 @@
  * Date: March 13th, 2015
  * Version: v0.1
  * Author: Ryan Protheroe
- * Description: makes sure you have correct amount of 
+ * Description: Makes sure you have the correct amount of 
  * nuts and washers based on number of bolts
  */
 package edu.hdsb.gwss.protheroe.ics3u.u4;
@@ -21,15 +21,15 @@ public class OrderChecker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        final int BOLT_PRICE = 5;
-        final int NUT_PRICE = 3;
-        final int WASHER_PRICE = 1;
+        final double BOLT_PRICE = 0.05;
+        final double NUT_PRICE = 0.03;
+        final double WASHER_PRICE = 0.01;
 
         //Scanner gets number of bolts, nuts and washers
         Scanner input = new Scanner(System.in);
         // Money format
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        
+
         int numberOfBolts;
         System.out.println("How many bolts are you purchasing?");
         numberOfBolts = input.nextInt();
@@ -42,18 +42,22 @@ public class OrderChecker {
 
         //Verifies you have the correct ratio of bolts:nuts, 1:1
         if (numberOfBolts > numberOfNuts) {
-            System.out.println("Check Order: Not enough nuts ");
+            System.out.println("Check Order: Not enough nuts, amount should be equivalent or larger than amount of bolts ");
         }
 
         //Verifies you have the correct ratio of washers:bolts, 2:1
         if (numberOfWashers < numberOfBolts) {
-            System.out.println("Check Order: Not enough washers");
+            System.out.println("Check Order: Not enough washers, amount should be double amount of bolts or greater ");
         }
 
         //Calculates price
+        double boltCost = numberOfBolts * BOLT_PRICE;
+        double nutCost = numberOfNuts * NUT_PRICE;
+        double washerCost = numberOfWashers * WASHER_PRICE;
+        double finalCost = boltCost + nutCost + washerCost;
         
-        
-        
+        //Outputs final cost to user
+        System.out.println("Final price :" + money.format(finalCost));
     }
 
 }

49b77e2bedec32e1770302ff91f4a7b4494340b0
1protheroery <1protheroery@gws4000112.ihdsb.ca>
2015-05-08 09:44:18 -0400

    - Completed binarySearch and average

--- src/edu/hdsb/gwss/protheroe/util/ArrayHelper.java
+++ src/edu/hdsb/gwss/protheroe/util/ArrayHelper.java
@@ -8,7 +8,6 @@
 package edu.hdsb.gwss.protheroe.util;
 
 import java.util.Scanner;
-
 /**
  *
  * @author 1protheroery
@@ -49,6 +48,7 @@ public class ArrayHelper {
         System.out.println("Bubble Sort Process");
         System.out.println("Number of Swaps: " + numberOfSwaps);
         System.out.println("Number of comparisons: " + numberOfComparisons);
+        System.out.println();
     }
 
     public static void selectionSort(int[] data) {
@@ -64,6 +64,7 @@ public class ArrayHelper {
         System.out.println("Selection Sort Process");
         System.out.println("Number of comparisons: " + numberOfComparisons);
         System.out.println("Number of Swaps: " + numberOfSwaps);
+        System.out.println();
     }
 
     public static void swap(int[] data, int i, int j) {
@@ -85,29 +86,72 @@ public class ArrayHelper {
     public static void linearSearch(int[] data) {
         int numberOfComparisons = 0;
         int check = 0;
+        int place;
 
         //OBJECTS
         Scanner input = new Scanner(System.in);
 
-        //USER SELECTS INITIAL OPTION
+        //USER ENTERS NUMBER TO SEARCH FOR
         System.out.println("Enter the value you wish to search for: ");
         int userChoice = input.nextInt();
         System.out.println();
 
+        //LOOP CHECKS FOR MATCH
         for (int counter = 0; counter < data.length; counter++) {
             numberOfComparisons++;
             if (data[counter] == userChoice) {
                 check++;
+                place = counter;
+                System.out.println("This value was found at " + (place + 1));
             }
         }
-        System.out.println("Search Process");
-        if (check == 1) {
-            System.out.println("This value was found " + check + " time");
-        } else if (check > 1) {
-            System.out.println("This value was found " + check + " times");
-        } else {
+        if (check == 0) {
             System.out.println("This value was found not found");
         }
         System.out.println("Number of comparisons: " + numberOfComparisons);
+    }
+
+    public static void binarySearch(int[] data) {
+        ArrayHelper.bubbleSort(data);
+
+        int place = -1;
+        int l = 0;
+        int r = data.length;
+        int mid;
+
+        //OBJECTS
+        Scanner input = new Scanner(System.in);
+
+        //USER ENTERS NUMBER TO SEARCH FOR
+        System.out.println("Enter the value you wish to search for: ");
+        int userChoice = input.nextInt();
+        System.out.println();
+
+        while (place == -1) {
+            if (l < r) {
+                mid = average((l + r), 2);
+                if (data[mid] > userChoice) {
+                    r = mid - 1;
+                } else if (data[mid] < userChoice) {
+                    l = mid + 1;
+                } else {
+                    place = mid;
+                    System.out.println("Value found at " + (place + 1));
+                }
+            } else {
+                System.out.println("Value not found");
+                place = 1;
+            }
+        }
+    }
+
+    public static int average(int i, int j) {
+        int average = i / j;
+        return average;
+    }
+
+    public static int sum(int i, int j) {
+        int sum = 0;
+        return sum;
     }
 }
